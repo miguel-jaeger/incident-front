17cfc0f7637607409477cca202a9a4ca4939bfe4
miguel-jaeger <miguel.jaeger@gmail.com>
2025-11-01 13:17:58 -0500

    actualizado para consumir el api desde render

--- tracker-app/src/config/config.js
+++ tracker-app/src/config/config.js
@@ -1,4 +1,4 @@
 // API URL SERVER
-//export const API_URL = 'https://api-incident.onrender.com/api/incidents';
+export const API_URL = 'https://api-incident.onrender.com/api/incidents';
 // API URL LOCAL
-export const API_URL = 'http://localhost:8080/api/incidents';
+//export const API_URL = 'http://localhost:8080/api/incidents';
