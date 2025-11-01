f65e59e4ffdd2dc35a795730894e791c0f8ad74b
miguel-jaeger <miguel.jaeger@gmail.com>
2025-11-01 12:40:49 -0500

    cambios en la configuracion

--- tracker-app/src/config/config.js
+++ tracker-app/src/config/config.js
@@ -1,1 +1,4 @@
-export const API_URL = 'https://api-incident.onrender.com/api/incidents';
+// API URL SERVER
+//export const API_URL = 'https://api-incident.onrender.com/api/incidents';
+// API URL LOCAL
+export const API_URL = 'http://localhost:8080/api/incidents';
